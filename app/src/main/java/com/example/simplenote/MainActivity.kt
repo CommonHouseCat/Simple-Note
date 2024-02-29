71ca59bce035ade0af61197a3fd65d42432975bc
Nguyen Quang Vinh <dev.nguyenquangvinh@gmail.com>
2024-02-29 22:56:28 +0700

    Working on the Note activity

--- app/src/main/java/com/example/simplenote/MainActivity.kt
+++ app/src/main/java/com/example/simplenote/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.simplenote
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var nextButton:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Reference the toolbar XML file
+        val toolbar:Toolbar = findViewById(R.id.toolbar)
+
+        // Set the toolbar as the app bar for the activity
+        setSupportActionBar(toolbar)
+
+        // Set the title and logo for this activity
+        supportActionBar?.title = " Note";
+        toolbar.setLogo(R.drawable.ic_add_new_note)
+
+        //
+        nextButton = findViewById(R.id.button)
+
+
     }
+
+    fun goToNoteActivity(view: View) {
+        val intent = Intent(this, Note_Activity::class.java)
+        startActivity(intent)
+    }
+
 }
