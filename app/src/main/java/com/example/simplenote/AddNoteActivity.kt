f0d36018321c04ebe0ea41d6bb0792e00f7a8e8f
Nguyen Quang Vinh <dev.nguyenquangvinh@gmail.com>
2024-03-05 23:31:12 +0700

    Adding recycle view text view xml

--- app/src/main/java/com/example/simplenote/AddNoteActivity.kt
+++ app/src/main/java/com/example/simplenote/AddNoteActivity.kt
@@ -1,22 +1,30 @@
 package com.example.simplenote
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.simplenote.databinding.ActivityMainBinding
+import com.example.simplenote.databinding.ActivityAddNoteBinding
 
 class AddNoteActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityAddNoteBinding
     private lateinit var db: NoteDBHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         db = NoteDBHelper(this)
 
-        binding.saveButton.setOnClick
-
+        binding.saveButton.setOnClickListener{
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val noteDT = NoteDT(0, title, content)
+            db.insertNote(noteDT)
+            finish()
+            Toast.makeText(this,"Note Saved",Toast.LENGTH_SHORT).show()
         }
+
     }
+}
