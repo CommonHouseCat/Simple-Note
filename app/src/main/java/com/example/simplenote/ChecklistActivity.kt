920e34f31bfdda2fda34c84281064f8b0ca26755
Nguyen Quang Vinh <dev.nguyenquangvinh@gmail.com>
2024-03-10 22:06:41 +0700

    Added the navigation drawer feature

---
+++ app/src/main/java/com/example/simplenote/ChecklistActivity.kt
@@ -0,0 +1,14 @@
+package com.example.simplenote
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class ChecklistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checklist)
+
+
+    }
+}
--- app/src/main/java/com/example/simplenote/MainActivity.kt
+++ app/src/main/java/com/example/simplenote/MainActivity.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.simplenote.databinding.ActivityMainBinding
 
@@ -13,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var db: NoteDBHelper
     private lateinit var notesAdapter: NotesAdapter
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +41,58 @@ class MainActivity : AppCompatActivity() {
 
         binding.notesRecycleView.layoutManager = LinearLayoutManager(this)
         binding.notesRecycleView.adapter = notesAdapter
+        drawerLayout = binding.drawerLayout
 
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, binding.toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.toolbar.setNavigationOnClickListener{
+            if(drawerLayout.isDrawerOpen(binding.navigationView)){
+                drawerLayout.closeDrawer(binding.navigationView)
+            }else{
+                drawerLayout.openDrawer(binding.navigationView)
+            }
+        }
+
+
+
+        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId){
+                R.id.nav_note -> {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    true
+                }
+                R.id.nav_checklist -> {
+                    startActivity(Intent(this, ChecklistActivity::class.java))
+                    true
+                }
+                R.id.nav_reminder -> {
+                    startActivity(Intent(this, ReminderActivity::class.java))
+                    true
+                }
+                R.id.nav_progress_tracker -> {
+                    startActivity(Intent(this, ProgressTrackerActivity::class.java))
+                    true
+                }
+                R.id.nav_settings -> {
+                    startActivity(Intent(this, SettingActivity::class.java))
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+        onBackPressedDispatcher.addCallback(this, object: OnBackPressedCallback(true){
+            override fun handleOnBackPressed(){
+                if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                }else{
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -48,4 +104,6 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
         notesAdapter.refreshData(db.getAllNotes())
     }
+
+
 }
